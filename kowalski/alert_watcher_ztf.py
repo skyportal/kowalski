6fe512e94c109f30b2ceb243be22dc3f74d0d6e4
Dmitry Duev <dmitry.duev@gmail.com>
2020-02-07 16:12:06 -0800

    wip: clean and lean

--- kowalski/alert_watcher_ztf.py
+++ kowalski/alert_watcher_ztf.py
@@ -24,7 +24,7 @@ from utils import deg2dms, deg2hms, great_circle_distance, in_ellipse, load_conf
 
 
 ''' load config and secrets '''
-config = load_config(config_file='config_kafka-listener.json')
+config = load_config(config_file='config_ingester.json')
 
 
 ''' Utilities for manipulating Avro data and schemas. '''
