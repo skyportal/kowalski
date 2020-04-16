a2b1b9c229703c6ba7611d74890976adea3d8a81
Dmitry Duev <dmitry.duev@gmail.com>
2020-04-16 16:44:40 -0700

    wip: tools for ztf mf

--- tools/ztf_mf2dump.py
+++ tools/ztf_mf2dump.py
@@ -22,45 +22,79 @@ if __name__ == '__main__':
 
     # cli argument - rc#: [0, 63] ? no, just iterate over range(0, 64) for the stuff below:
     for rc in range(0, 1):
-        # # fetch matchfiles from gs://ztf-matchfiles-t_tag/rc/ to /_tmp/ztf-matchfiles-t_tag/
-        # subprocess.run([
-        #     "docker", "exec", "-it", "kowalski_ingester_1", #"/bin/bash", "-c",
-        #     "/usr/local/bin/gsutil",
-        #     # "-m", "cp",
-        #     "cp",
-        #     f"gs://ztf-matchfiles-{args.tag}/{rc}/ztf_000245_zg_c01_q1_match.pytable",  # test
-        #     # f"gs://ztf-matchfiles-{t_tag}/{rc}/*.pytable",
-        #     f"/_tmp/ztf_matchfiles_{args.tag}/",
-        # ])
-        # # run ingest_ztf_matchfiles.py
-        # subprocess.run([
-        #     "docker", "exec", "-it", "kowalski_ingester_1",
-        #     "python",
-        #     "/app/ingest_ztf_matchfiles.py",
-        #     "--rm",
-        #     "--tag", args.tag,
-        #     "--np", str(args.np),
-        #     "--bs", str(args.bs),
-        # ])
-        # # dump to /_tmp/
-        # with open(f"/home/dmitryduev/tmp/ZTF_sources_{args.tag}.rc{rc:02d}.dump", 'w') as f:
-        #     subprocess.run([
-        #         "docker", "exec", "kowalski_mongo_1",
-        #         f"mongodump", "-u=mongoadmin", "-p=mongoadminsecret", "--authenticationDatabase=admin",
-        #         "--archive", "--db=kowalski", f"--collection=ZTF_sources_{args.tag}",
-        #     ], stdout=f)
-        # # lbzip2 the dump
-        # subprocess.run([
-        #     "lbzip2", "-v", "-f",
-        #     "-n", str(args.np),
-        #     f"/home/dmitryduev/tmp/ZTF_sources_{args.tag}.rc{rc:02d}.dump"
-        # ])
+        # fetch matchfiles from gs://ztf-matchfiles-t_tag/rc/ to /_tmp/ztf-matchfiles-t_tag/
+        subprocess.run([
+            "docker", "exec", "-it", "kowalski_ingester_1",
+            "/usr/local/bin/gsutil",
+            # "-m", "cp",
+            "cp",
+            f"gs://ztf-matchfiles-{args.tag}/{rc}/ztf_000245_zg_c01_q1_match.pytable",  # test
+            # f"gs://ztf-matchfiles-{t_tag}/{rc}/*.pytable",
+            f"/_tmp/ztf_matchfiles_{args.tag}/",
+        ])
+        # run ingest_ztf_matchfiles.py
+        subprocess.run([
+            "docker", "exec", "-it", "kowalski_ingester_1",
+            "python",
+            "/app/ingest_ztf_matchfiles.py",
+            "--rm",
+            "--tag", args.tag,
+            "--np", str(args.np),
+            "--bs", str(args.bs),
+        ])
+        # dump to /_tmp/
+        with open(f"/home/dmitryduev/tmp/ZTF_sources_{args.tag}.rc{rc:02d}.dump", 'w') as f:
+            subprocess.run([
+                "docker", "exec", "kowalski_mongo_1",
+                f"mongodump", "-u=mongoadmin", "-p=mongoadminsecret", "--authenticationDatabase=admin",
+                "--archive", "--db=kowalski", f"--collection=ZTF_sources_{args.tag}",
+            ], stdout=f)
+        # lbzip2 the dump
+        subprocess.run([
+            "lbzip2", "-v", "-f",
+            "-n", str(args.np),
+            f"/home/dmitryduev/tmp/ZTF_sources_{args.tag}.rc{rc:02d}.dump"
+        ])
         # mv to gs://ztf-sources-20200401
+        subprocess.run([
+            "docker", "exec", "-it", "kowalski_ingester_1",
+            "/usr/local/bin/gsutil",
+            "mv",
+            f"/_tmp/ZTF_sources_{args.tag}.rc{rc:02d}.dump.bz2"
+            f"gs://ztf-sources-{args.tag}/",
+        ])
         # drop the sources collection, keep the exposures collection
         subprocess.run([
             "docker", "exec", "kowalski_mongo_1",
             "mongo", "-u", "mongoadmin", "-p", "mongoadminsecret", "--authenticationDatabase", "admin",
             "kowalski", "--eval", f"db.ZTF_sources_{args.tag}.drop()"
         ])
-        # export exposures
-        pass
+
+    # export exposures
+    # dump to /_tmp/
+    with open(f"/home/dmitryduev/tmp/ZTF_exposures_{args.tag}.dump", 'w') as f:
+        subprocess.run([
+            "docker", "exec", "kowalski_mongo_1",
+            f"mongodump", "-u=mongoadmin", "-p=mongoadminsecret", "--authenticationDatabase=admin",
+            "--archive", "--db=kowalski", f"--collection=ZTF_exposures_{args.tag}",
+        ], stdout=f)
+    # lbzip2 the dump
+    subprocess.run([
+        "lbzip2", "-v", "-f",
+        "-n", str(args.np),
+        f"/home/dmitryduev/tmp/ZTF_exposures_{args.tag}.dump"
+    ])
+    # mv to gs://ztf-sources-20200401
+    subprocess.run([
+        "docker", "exec", "-it", "kowalski_ingester_1",
+        "/usr/local/bin/gsutil",
+        "mv",
+        f"/_tmp/ZTF_exposures_{args.tag}.dump.bz2"
+        f"gs://ztf-sources-{args.tag}/",
+    ])
+    # drop the exposures collection
+    subprocess.run([
+        "docker", "exec", "kowalski_mongo_1",
+        "mongo", "-u", "mongoadmin", "-p", "mongoadminsecret", "--authenticationDatabase", "admin",
+        "kowalski", "--eval", f"db.ZTF_exposures_{args.tag}.drop()"
+    ])
