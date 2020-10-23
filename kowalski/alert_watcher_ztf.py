9fcfd02f9ab9191394fbf1242bef3b3e1c20b15d
dmitryduev <dmitry.duev@gmail.com>
2020-10-23 00:01:10 -0700

    filter bad diffmaglim

--- kowalski/alert_watcher_ztf.py
+++ kowalski/alert_watcher_ztf.py
@@ -132,6 +132,10 @@ def make_photometry(alert: dict, jd_start: float = None):
         .sort_values(by=['mjd'])
     )
 
+    # filter out old bad data:
+    w_bad_diffmaglim = df_light_curve["diffmaglim"] > 0
+    df_light_curve = df_light_curve.loc[w_bad_diffmaglim]
+
     # only "new" photometry requested?
     if jd_start is not None:
         w_after_jd = df_light_curve['jd'] > jd_start
