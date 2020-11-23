1baae1211440951d9a45460aaf5226a9d0117515
dmitryduev <dmitry.duev@gmail.com>
2020-11-23 00:20:26 -0800

    fix linting

--- tools/generate_secrets.py
+++ tools/generate_secrets.py
@@ -2,17 +2,17 @@ from cryptography import fernet
 from utils import uid
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     secrets = dict()
 
     fernet_key = fernet.Fernet.generate_key().decode()
     aiohttp_secret_key = uid(32)
     jwt_secret_key = uid(32)
 
-    for key in ('server', 'misc'):
+    for key in ("server", "misc"):
         if key not in secrets:
             secrets[key] = dict()
 
-    secrets['server']['SECRET_KEY'] = aiohttp_secret_key
-    secrets['server']['JWT_SECRET_KEY'] = jwt_secret_key
-    secrets['misc']['fernet_key'] = fernet_key
+    secrets["server"]["SECRET_KEY"] = aiohttp_secret_key
+    secrets["server"]["JWT_SECRET_KEY"] = jwt_secret_key
+    secrets["misc"]["fernet_key"] = fernet_key
