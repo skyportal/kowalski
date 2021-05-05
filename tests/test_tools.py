a993a3209ccd9ebc04d6091f45519a3d3234faeb
kshin <kshin@caltech.edu>
2021-05-05 15:51:10 -0400

    Add test matchfiles

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -3,6 +3,7 @@ import pytest
 from ingest_ztf_source_features import run as run_ztf_source_features
 from ingest_vlass import run as run_vlass
 from ingest_igaps import run as run_igaps
+from ingest_ztf_matchfiles import run as run_ztf_matchfiles
 from utils import get_default_args, load_config, log, Mongo
 
 
@@ -47,6 +48,26 @@ class TestTools:
         log(f"Ingested features of {len(ingested_entries)} sources")
 
         assert len(ingested_entries) == 123
+
+    def test_ingest_ztf_matchfiles(self):
+        tag = "20201201"
+        sources_collection = f"ZTF_sources_{tag}"
+        exposures_collection = f"ZTF_exposures_{tag}"
+        run_ztf_matchfiles(
+            path="/data",
+            tag="20201201",
+            num_processes=1,
+        )
+
+        ingested_sources = list(self.mongo.db[sources_collection].find({}, {"_id": 1}))
+        ingested_exposures = list(
+            self.mongo.db[exposures_collection].find({}, {"_id": 1})
+        )
+        log(f"Ingested lightcurves for {len(ingested_sources)} sources")
+        log(f"Ingested {len(ingested_exposures)} exposures")
+
+        assert len(ingested_sources) == 16
+        assert len(ingested_exposures) == 10
 
     def test_ingest_vlass(self):
 
