525db1f119cc80279718e5e99105cf00f46e06a7
Michael Coughlin <cough052@umn.edu>
2021-09-14 13:00:32 -0500

    first public catalog example (#109)
    * first public catalog example
    
    * fix test
    
    * Correct number of alerts
    
    Co-authored-by: Michael Coughlin <coughlin@physics.harvard.edu>

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -4,6 +4,7 @@ from random import randrange
 from ingest_ztf_source_features import run as run_ztf_source_features
 from ingest_vlass import run as run_vlass
 from ingest_igaps import run as run_igaps
+from ingest_ztf_public import run as run_ztf_public
 from ingest_ztf_matchfiles import run as run_ztf_matchfiles
 from ingest_ptf_matchfiles import run as run_ptf_matchfiles
 from utils import get_default_args, load_config, log, Mongo
@@ -98,6 +99,17 @@ class TestTools:
         log(f"Ingested features of {len(ingested_entries)} sources")
 
         assert len(ingested_entries) == 100
+
+    def test_ingest_ztf_public(self):
+        tag = get_default_args(run_ztf_public).get("tag")
+        collection = f"ZTF_public_sources_{tag}"
+
+        run_ztf_public(path="/app/data/catalogs", num_proc=1)
+
+        ingested_entries = list(self.mongo.db[collection].find({}, {"_id": 1}))
+        log(f"Ingested features of {len(ingested_entries)} sources")
+
+        assert len(ingested_entries) == 5449
 
     def test_ingest_ptf(self):
 
