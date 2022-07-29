94130aca6ef6f21a67d5baad3b2d7ad886b13ce5
sukishore12 <sukishore12@gmail.com>
2022-07-29 16:26:18 -0700

    reads stream with dask (no mongo in poll)

--- kowalski/alert_broker_winter.py
+++ kowalski/alert_broker_winter.py
@@ -53,12 +53,6 @@ class WNTRAlertConsumer(AlertConsumer, ABC):
     """
    
     def __init__(self, topic:str, dask_client: dask.distributed.Client, **kwargs):
-        # Old class params
-        # bootstrap_server_str:str, 
-        # topic:str,
-        # group_id:str,
-        # dask_client: dask.distributed.Client,
-        # verbose = 2,
         """
         Initializes Kafka consumer.
 
@@ -73,135 +67,9 @@ class WNTRAlertConsumer(AlertConsumer, ABC):
         :type verbose: int, optional
         """
         super().__init__(topic, dask_client, **kwargs)
-
-        # # Configure consumer connection to Kafka broker
-        # bootstrap_servers = {bootstrap_server_str} 
-        # logger.info(f'testingggggg')
-        # conf = {
-        #     "bootstrap.servers": bootstrap_servers,
-        #     "default.topic.config": {"auto.offset.reset": "earliest"},
-        # }
-
-        # conf["group.id"] = group_id
-        # # make it unique
-        # conf["group.id"] = f"{conf['group.id']}_{datetime.datetime.utcnow().strftime('%Y-%m-%d_%H:%M:%S.%f')}"
-        
-        # # keep track of disconnected partitions
-        # self.num_disconnected_partitions = 0
-        # self.topic = topic
-        # self.verbose = 2
-
-        # def error_cb(err, _self=self):
-        #     print(f"error_cb --------> {err}")
-        #     print(f'{err.code()}')
-        #     if err.code() == -195:
-        #         _self.num_disconnected_partitions += 1
-        #         if _self.num_disconnected_partitions == _self.num_partitions:
-        #             print("All partitions got disconnected, killing thread")
-        #             sys.exit()
-        #         else:
-        #             print(
-        #                 f"{_self.topic}: disconnected from partition. total: {_self.num_disconnected_partitions}"
-        #             )
-
-        # self.consumer = confluent_kafka.Consumer(conf)
-        # self.num_partitions = 0
-
-        # def on_assign(consumer, partitions, _self=self):
-        #     # force-reset offsets when subscribing to a topic:
-        #     print(f'parititions: {partitions}')
-        #     for part in partitions:
-        #         # -2 stands for beginning and -1 for end
-        #         part.offset = -2
-        #         # keep number of partitions.
-        #         # when reaching end of last partition, kill thread and start from beginning
-        #         _self.num_partitions += 1
-        #         print(f'Topic partition offsets: {consumer.get_watermark_offsets(part)}')
-
-        # self.consumer.subscribe([topic], on_assign=on_assign)
-        # print(f"Successfully subscribed to {topic}")
-
-    @staticmethod
-    def read_schema_data(bytes_io):
-        """
-        Read data that already has an Avro schema.
-
-        :param bytes_io: `_io.BytesIO` Data to be decoded.
-        :return: `dict` Decoded data.
-        """
-        bytes_io.seek(0)
-        message = fastavro.reader(bytes_io)
-        return message
-
-    @classmethod
-    def decode_message(cls, msg):
-        """
-        Decode Avro message according to a schema.
-
-        :param msg: The Kafka message result from consumer.poll()
-        :return:
-        """
-        message = msg.value()
-        decoded_msg = message
-
-        try:
-            bytes_io = io.BytesIO(message)
-            decoded_msg = cls.read_schema_data(bytes_io)
-        except AssertionError:
-            decoded_msg = None
-        except IndexError:
-            literal_msg = literal_eval(
-                str(message, encoding="utf-8")
-            )  # works to give bytes
-            bytes_io = io.BytesIO(literal_msg)  # works to give <class '_io.BytesIO'>
-            decoded_msg = cls.read_schema_data(bytes_io)  # yields reader
-        except Exception:
-            decoded_msg = message
-        finally:
-            return decoded_msg
-        
-    def alert_mongify(self, alert: Mapping):
-        """
-        Prepare a raw alert for ingestion into MongoDB:
-          - add a placeholder for ML-based classifications
-          - add coordinates for 2D spherical indexing and compute Galactic coordinates
-          - cut off the prv_candidates section
-
-        :param alert:
-        :return:
-        """
-        doc = dict(alert)
-        # placeholders for classifications
-        doc["classifications"] = dict()
-
-        # GeoJSON for 2D indexing
-        doc["coordinates"] = {}
-        _ra = doc["candidate"]["ra"]
-        _dec = doc["candidate"]["dec"]
-        # string format: H:M:S, D:M:S
-        _radec_str = [deg2hms(_ra), deg2dms(_dec)]
-        doc["coordinates"]["radec_str"] = _radec_str
-        # for GeoJSON, must be lon:[-180, 180], lat:[-90, 90] (i.e. in deg)
-        _radec_geojson = [_ra - 180.0, _dec]
-        doc["coordinates"]["radec_geojson"] = {
-            "type": "Point",
-            "coordinates": _radec_geojson,
-        }
-
-        # Galactic coordinates l and b
-        l, b = radec2lb(doc["candidate"]["ra"], doc["candidate"]["dec"])
-        doc["coordinates"]["l"] = l
-        doc["coordinates"]["b"] = b
-
-        prv_candidates = deepcopy(doc["prv_candidates"])
-        doc.pop("prv_candidates", None)
-        if prv_candidates is None:
-            prv_candidates = []
-
-        return doc, prv_candidates
-
     
-    def process_alert(self, alert: Mapping, topic: str):
+    @staticmethod
+    def process_alert(alert: Mapping, topic: str):
         """
         Main function that runs on a single alert.
             -Read top-level packet field
@@ -211,6 +79,7 @@ class WNTRAlertConsumer(AlertConsumer, ABC):
         :param topic: Kafka stream topic name for bookkeeping
         :return:
         """
+        print(f'In process alert')
         candid = alert["candid"]
         object_id = alert["objectId"]
 
@@ -231,6 +100,7 @@ class WNTRAlertConsumer(AlertConsumer, ABC):
 
         # candid not in db, ingest decoded avro packet into db
         with timer(f"Mongification of {object_id} {candid}"):
+            print(f'Trying to mongify')
             alert, prv_candidates = self.alert_mongify(alert)
 
         # prv_candidates: pop nulls - save space
@@ -239,50 +109,9 @@ class WNTRAlertConsumer(AlertConsumer, ABC):
             for prv_candidate in prv_candidates
         ]
 
-    def poll(self):
-        """
-        Polls Kafka broker to consume a topic.
-        If receives message, for each packet:
-            - decodes avro schema
-            - processess alert (#TODO)
-        
-        """
-        msg = self.consumer.poll()
-
-        if msg is None:
-            print("############################")
-            print("Caught error: msg is None")
-            print("############################")
-
-        if msg.error():
-            # reached end of topic
-            print(f"Caught error: {msg.error()}")
-
-        elif msg is not None:
-            try:
-                # decode avro packet
-                with timer("Decoding alert", self.verbose > 1):
-                    msg_decoded = self.decode_message(msg)
-                    print(f'Avro packet decoded')
-
-                for record in msg_decoded:
-                        with timer(
-                            f"Submitting alert {record['objectId']} {record['candid']} for processing",
-                            self.verbose > 1,
-                        ):
-                            self.process_alert(record, self.topic)
-                            print(f"Finished process_alert {record['objectId']} {record['candid']}")
-
-
-            except Exception as e:
-                print("Error in poll!")
-                print(e)
-                _err = traceback.format_exc()
-                print(_err)
-
 class WNTRAlertWorker(AlertWorker, ABC):
     def __init__(self, **kwargs):
-        super().__init__(instrument="PGIR", **kwargs)
+        super().__init__(instrument="WNTR", **kwargs)
         
 class WorkerInitializer(dask.distributed.WorkerPlugin):
     def __init__(self, *args, **kwargs):
